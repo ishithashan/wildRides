f42338cdc116881a74a60b56b1b209fc34d7fe63
ishithashan <ishitha.shanm@gmail.com>
2026-03-15 11:31:14 +0530

    Update config.js
    changed cognito keys

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_Rhk4yOeKF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7q3nioddj61vaivn8ndtdle657', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '42usfc3t5e4suca01q9rh1uc7o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
