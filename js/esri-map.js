1b4c1eeaad310f8822df460e590bb243b440cc9f
ishithashan <ishitha.shanm@gmail.com>
2026-03-17 00:52:56 +0530

    feature added (Pickup & drop + Ride History + AWS Lambda GET updates)

--- js/esri-map.js
+++ js/esri-map.js
@@ -13,12 +13,13 @@ WildRydes.map = WildRydes.map || {};
         'esri/symbols/PictureMarkerSymbol',
         'esri/geometry/support/webMercatorUtils',
         'dojo/domReady!'
-    ], function requireCallback(
+    ], function (
         Map, MapView,
         Graphic, Point, TextSymbol,
         PictureMarkerSymbol, webMercatorUtils
     ) {
         var wrMap = WildRydes.map;
+        wrMap.clearGraphics = clearGraphics;
 
         var map = new Map({ basemap: 'gray-vector' });
 
@@ -29,13 +30,17 @@ WildRydes.map = WildRydes.map || {};
             zoom: 15
         });
 
-        var pinSymbol = new TextSymbol({
+        // Symbols
+        var pickupSymbol = new TextSymbol({
             color: '#07373d',
             text: '\ue61d',
-            font: {
-                size: 20,
-                family: 'CalciteWebCoreIcons'
-            }
+            font: { size: 24, family: 'CalciteWebCoreIcons' }
+        });
+
+        var dropoffSymbol = new TextSymbol({
+            color: '#d32f2f',
+            text: '\ue61d',
+            font: { size: 24, family: 'CalciteWebCoreIcons' }
         });
 
         var unicornSymbol = new PictureMarkerSymbol({
@@ -44,99 +49,117 @@ WildRydes.map = WildRydes.map || {};
             height: '50px'
         });
 
-        var pinGraphic;
-        var unicornGraphic;
+        // Graphics references
+        var pickupGraphic = null;
+        var dropoffGraphic = null;
+        var unicornGraphic = null;
 
-        function updateCenter(newValue) {
-            wrMap.center = {
-                latitude: newValue.latitude,
-                longitude: newValue.longitude
-            };
+        // Current mode
+        wrMap.clickMode = 'pickup';  // default
+
+        function clearGraphics(type = 'all') {
+            if (type === 'all' || type === 'pickup') {
+                if (pickupGraphic) {
+                    view.graphics.remove(pickupGraphic);
+                    pickupGraphic = null;
+                }
+            }
+            if (type === 'all' || type === 'dropoff') {
+                if (dropoffGraphic) {
+                    view.graphics.remove(dropoffGraphic);
+                    dropoffGraphic = null;
+                }
+            }
+            if (type === 'all' || type === 'unicorn') {
+                if (unicornGraphic) {
+                    view.graphics.remove(unicornGraphic);
+                    unicornGraphic = null;
+                }
+            }
         }
 
-        function updateExtent(newValue) {
-            var min = webMercatorUtils.xyToLngLat(newValue.xmin, newValue.ymin);
-            var max = webMercatorUtils.xyToLngLat(newValue.xmax, newValue.ymax);
-            wrMap.extent = {
-                minLng: min[0],
-                minLat: min[1],
-                maxLng: max[0],
-                maxLat: max[1]
-            };
-        }
-
-        view.watch('extent', updateExtent);
-        view.watch('center', updateCenter);
-        view.then(function onViewLoad() {
-            updateExtent(view.extent);
-            updateCenter(view.center);
-        });
-
-        /*view.on('click', function handleViewClick(event) {
-            wrMap.selectedPoint = event.mapPoint;
-            view.graphics.remove(pinGraphic);
-            pinGraphic = new Graphic({
-                symbol: pinSymbol,
-                geometry: wrMap.selectedPoint
-            });
-            view.graphics.add(pinGraphic);
-            $(wrMap).trigger('pickupChange');
-        });*/
-        view.on('click', function handleViewClick(event) {
-            wrMap.selectedPoint = event.mapPoint;
-            view.graphics.remove(pinGraphic);
-            pinGraphic = new Graphic({
-                symbol: pinSymbol,
-                geometry: wrMap.selectedPoint
-            });
-            view.graphics.add(pinGraphic);
-            var lat = event.mapPoint.latitude;
-            var lon = event.mapPoint.longitude;
-            fetch("https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?location="
-                + lon + "," + lat + "&f=pjson")
-                .then(response => response.json())
+        function reverseGeocode(lon, lat, callback) {
+            fetch(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode?location=${lon},${lat}&f=pjson`)
+                .then(r => r.json())
                 .then(data => {
+                    let name = "Unknown location";
                     if (data.address) {
-                        wrMap.locationName =
-                            data.address.Neighborhood ||
-                            data.address.District ||
-                            data.address.Subregion ||
-                            data.address.City ||
-                            "Unknown location";
-                    } else {
-                        wrMap.locationName = "Unknown location";
+                        name = data.address.Neighborhood ||
+                               data.address.District ||
+                               data.address.Subregion ||
+                               data.address.City ||
+                               name;
                     }
-                    $(wrMap).trigger('pickupChange');
+                    callback(name);
                 })
-                .catch(() => {
-                    wrMap.locationName = "Unknown location";
-                    $(wrMap).trigger('pickupChange');
+                .catch(() => callback("Unknown location"));
+        }
+
+        // Map click handler
+        view.on('click', function (event) {
+            console.log("Map clicked → current mode:", wrMap.clickMode); // debug
+
+            if (wrMap.clickMode !== 'pickup' && wrMap.clickMode !== 'dropoff') {
+                console.log("Ignoring click — no valid mode set");
+                return;
+            }
+
+            var point = event.mapPoint;
+            var lat = point.latitude;
+            var lon = point.longitude;
+
+            if (wrMap.clickMode === 'pickup') {
+                clearGraphics('pickup');
+                reverseGeocode(lon, lat, (name) => {
+                    wrMap.pickup = { latitude: lat, longitude: lon, name };
+                    pickupGraphic = new Graphic({ geometry: point, symbol: pickupSymbol });
+                    view.graphics.add(pickupGraphic);
+                    $(wrMap).trigger('locationChange');
+                    console.log("Pickup set:", wrMap.pickup);
                 });
+            } else if (wrMap.clickMode === 'dropoff') {
+                clearGraphics('dropoff');
+                reverseGeocode(lon, lat, (name) => {
+                    wrMap.dropoff = { latitude: lat, longitude: lon, name };
+                    dropoffGraphic = new Graphic({ geometry: point, symbol: dropoffSymbol });
+                    view.graphics.add(dropoffGraphic);
+                    $(wrMap).trigger('locationChange');
+                    console.log("Dropoff set:", wrMap.dropoff);
+                });
+            }
         });
 
-        wrMap.animate = function animate(origin, dest, callback) {
+        // Mode setters
+        wrMap.setPickupMode = function () {
+            wrMap.clickMode = 'pickup';
+            console.log("Mode → pickup"); // debug
+            $(wrMap).trigger('modeChange');
+        };
+
+        wrMap.setDropoffMode = function () {
+            wrMap.clickMode = 'dropoff';
+            console.log("Mode → dropoff"); // debug
+            $(wrMap).trigger('modeChange');
+        };
+
+        // Animation
+        wrMap.animate = function (origin, dest, callback) {
+            console.log("animate called", origin, dest);
             var startTime;
-            var step = function animateFrame(timestamp) {
-                var progress;
-                var progressPct;
-                var point;
-                var deltaLat;
-                var deltaLon;
+            var step = function (timestamp) {
                 if (!startTime) startTime = timestamp;
-                progress = timestamp - startTime;
-                progressPct = Math.min(progress / 2000, 1);
-                deltaLat = (dest.latitude - origin.latitude) * progressPct;
-                deltaLon = (dest.longitude - origin.longitude) * progressPct;
-                point = new Point({
-                    longitude: origin.longitude + deltaLon,
-                    latitude: origin.latitude + deltaLat
+                var progress = timestamp - startTime;
+                var progressPct = Math.min(progress / 2000, 1);
+
+                var point = new Point({
+                    longitude: origin.longitude + (dest.longitude - origin.longitude) * progressPct,
+                    latitude: origin.latitude + (dest.latitude - origin.latitude) * progressPct
                 });
+
                 view.graphics.remove(unicornGraphic);
-                unicornGraphic = new Graphic({
-                    geometry: point,
-                    symbol: unicornSymbol
-                });
+                unicornGraphic = new Graphic({ geometry: point, symbol: unicornSymbol });
                 view.graphics.add(unicornGraphic);
+
                 if (progressPct < 1) {
                     requestAnimationFrame(step);
                 } else {
@@ -146,8 +169,28 @@ WildRydes.map = WildRydes.map || {};
             requestAnimationFrame(step);
         };
 
-        wrMap.unsetLocation = function unsetLocation() {
-            view.graphics.remove(pinGraphic);
+        wrMap.unsetLocation = function () {
+            clearGraphics('all');
         };
+
+        // View watchers
+        function updateCenter(newValue) {
+            wrMap.center = { latitude: newValue.latitude, longitude: newValue.longitude };
+        }
+
+        function updateExtent(newValue) {
+            var min = webMercatorUtils.xyToLngLat(newValue.xmin, newValue.ymin);
+            var max = webMercatorUtils.xyToLngLat(newValue.xmax, newValue.ymax);
+            wrMap.extent = { minLng: min[0], minLat: min[1], maxLng: max[0], maxLat: max[1] };
+        }
+
+        view.watch('extent', updateExtent);
+        view.watch('center', updateCenter);
+
+        view.then(() => {
+            console.log("Map view ready");
+            updateExtent(view.extent);
+            updateCenter(view.center);
+        });
     });
 }(jQuery));
--- js/ride.js
+++ js/ride.js
@@ -15,139 +15,287 @@ WildRydes.map = WildRydes.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
-    function requestUnicorn(pickupLocation) {
+
+    function requestUnicorn(pickup, dropoff) {
         $.ajax({
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
-            headers: {
-                Authorization: authToken
-            },
+            headers: { Authorization: authToken },
             data: JSON.stringify({
                 PickupLocation: {
-                    Latitude: pickupLocation.latitude,
-                    Longitude: pickupLocation.longitude
+                    Latitude: pickup.latitude,
+                    Longitude: pickup.longitude
+                },
+                DropoffLocation: {  // ← new!
+                    Latitude: dropoff.latitude,
+                    Longitude: dropoff.longitude
                 }
             }),
             contentType: 'application/json',
             success: completeRequest,
-            error: function ajaxError(jqXHR, textStatus, errorThrown) {
-                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
-                console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when requesting your car:\n' + jqXHR.responseText);
+            error: function ajaxError(jqXHR) {
+                console.error('Error requesting ride: ', jqXHR);
+                alert('Error requesting ride:\n' + jqXHR.responseText);
             }
         });
     }
 
     function completeRequest(result) {
-        var unicorn;
-        var pronoun;
-        console.log('Response received from API: ', result);
-        unicorn = result.Unicorn;
-        pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
-        displayUpdate('Driver: ' + unicorn.Name + ', your car (' + unicorn.CarNumber + ') is on ' + pronoun + ' way.');
-        animateArrival(function animateCallback() {
-            displayUpdate(unicorn.Name + ' has arrived. Hop up!');
-            WildRydes.map.unsetLocation();
-            $('#request').prop('disabled', 'disabled');
-            $('#request').text('Set Pickup');
+        var unicorn = result.Unicorn;
+        var pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+        displayUpdate(`Driver: ${unicorn.Name}, your car (${unicorn.CarNumber}) is on ${pronoun} way to pickup.`);
+        animateArrival(function () {
+            displayUpdate(`${unicorn.Name} has arrived at pickup. Heading to drop-off!`);
+            //WildRydes.map.unsetLocation();
+            $('#request').prop('disabled', true).text('Ride Requested');
         });
     }
 
-    // Register click handler for #request button
-    $(function onDocReady() {
-        $('#request').click(handleRequestClick);
-        $(WildRydes.map).on('pickupChange', handlePickupChanged);
-
-        WildRydes.authToken.then(function updateAuthMessage(token) {
-            if (token) {
-                displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
-                $('.authToken').text(token);
-            }
-        });
-
-        if (!_config.api.invokeUrl) {
-            $('#noApiMessage').show();
-        }
-    });
-
     function calculateDistance(lat1, lon1, lat2, lon2) {
-        var R = 6371; // km
-        var dLat = (lat2 - lat1) * Math.PI / 180;
-        var dLon = (lon2 - lon1) * Math.PI / 180;
-
-        var a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1 * Math.PI / 180) *
-            Math.cos(lat2 * Math.PI / 180) *
-            Math.sin(dLon / 2) *
-            Math.sin(dLon / 2);
-
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
+        const R = 6371; // km
+        const dLat = (lat2 - lat1) * Math.PI / 180;
+        const dLon = (lon2 - lon1) * Math.PI / 180;
+        const a = Math.sin(dLat / 2) ** 2 +
+                  Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+                  Math.sin(dLon / 2) ** 2;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return R * c;
     }
 
     function estimateFare(distance) {
-        var baseFare = 50;     // ₹
-        var perKm = 12;        // ₹ per km
+        const baseFare = 50;
+        const perKm = 12;
+        return Math.round(baseFare + (distance * perKm));}
 
-        return baseFare + (distance * perKm);
+function updateUIWithLocationsAndMode() {
+    const pickup = WildRydes.map.pickup;
+    const dropoff = WildRydes.map.dropoff;
+    const mode = WildRydes.map.clickMode;
+
+    // Clear previous messages (optional - comment out if you want history)
+    // $('#updates').empty();
+
+    let message = "";
+
+    if (!pickup && !dropoff) {
+        message = "Welcome! Use the buttons to set your pickup and drop-off locations.";
     }
 
-    /*function handlePickupChanged() {
-        var requestButton = $('#request');
-        requestButton.text('Request Car');
-        requestButton.prop('disabled', false);
-    }*/
-   function handlePickupChanged() {
-        var pickup = WildRydes.map.selectedPoint;
-        var baseLat = 13.0096;
-        var baseLon = 80.0043;
-
-        var distance = calculateDistance(
-            baseLat,
-            baseLon,
-            pickup.latitude,
-            pickup.longitude
-        );
-
-        var fare = estimateFare(distance);
-
-        displayUpdate(
-            "Distance: " + distance.toFixed(2) +
-            " km | Estimated Fare: ₹" + Math.round(fare) + 
-            " | Pickup location: " + WildRydes.map.locationName
-        );
-
-        var requestButton = $('#request');
-        requestButton.text('Request Car');
-        requestButton.prop('disabled', false);
+    if (pickup) {
+        message += `<strong>Pickup Point:</strong> ${pickup.name} (${pickup.latitude.toFixed(4)}, ${pickup.longitude.toFixed(4)})<br>`;
     }
+
+    if (pickup && dropoff) {
+        const distance = calculateDistance(
+            pickup.latitude, pickup.longitude,
+            dropoff.latitude, dropoff.longitude
+        );
+        const fare = estimateFare(distance);
+        message += `<strong>Drop Point:</strong> Distance: ${distance.toFixed(2)} km | Estimated Fare: ₹${fare} | Drop location: ${dropoff.name}<br>`;
+        $('#request').prop('disabled', false).text('Request Ride');
+    } else if (pickup) {
+        message += "Now set your drop-off location on the map.<br>";
+        $('#request').prop('disabled', true).text('Set Drop-off first');
+    } else {
+        message += "First set your pickup location.<br>";
+        $('#request').prop('disabled', true).text('Set Pickup first');
+    }
+
+    // Only show current mode when relevant
+    if (pickup && !dropoff) {
+        message += `<small>Current mode: Setting Drop-off (click "Set Drop-off Location" if needed)</small>`;
+    } else if (!pickup) {
+        message += `<small>Current mode: Setting Pickup</small>`;
+    }
+
+    displayUpdate(message);
+}
+
+$(function onDocReady() {
+    // Buttons to switch mode
+    $('#setPickup').click(() => {
+        WildRydes.map.setPickupMode();
+        updateUIWithLocationsAndMode();  // ← call here
+    });
+
+    $('#setDropoff').click(() => {
+        WildRydes.map.setDropoffMode();
+        updateUIWithLocationsAndMode();  // ← call here
+    });
+
+    $('#request').click(handleRequestClick);
+
+    // Listen for both events
+    $(WildRydes.map).on('locationChange modeChange', updateUIWithLocationsAndMode);
+
+    WildRydes.authToken.then(token => {
+        if (token) {
+            displayUpdate('Authenticated. Use buttons to set pickup/drop-off on map.');
+        }
+    });
+
+    if (!_config.api.invokeUrl) {
+        $('#noApiMessage').show();
+    }
+
+    // Initial UI state
+    updateUIWithLocationsAndMode();
+
+    $('#resetLocations').click(() => {
+    WildRydes.map.pickup = null;
+    WildRydes.map.dropoff = null;
+    WildRydes.map.unsetLocation();
+    updateUIWithLocationsAndMode();
+    displayUpdate("Locations cleared. Start over by setting pickup.");
+});
+$('#newRide').click(() => {
+    WildRydes.map.pickup = null;
+    WildRydes.map.dropoff = null;
+    WildRydes.map.clickMode = 'pickup';
+    WildRydes.map.unsetLocation();           // clears pins + unicorn
+    $('#request').prop('disabled', true).text('Set Pickup first');
+    $('#newRide').hide();
+    displayUpdate("Map cleared! Ready for a new ride.<br>Click 'Set Pickup Location' to begin.");
+    updateUIWithLocationsAndMode();
+});
+$('#showHistory').click(async function () {
+    displayUpdate('<em>Loading your ride history...</em>');
+
+    try {
+        const userPool = new AmazonCognitoIdentity.CognitoUserPool({
+            UserPoolId: _config.cognito.userPoolId,
+            ClientId: _config.cognito.userPoolClientId
+        });
+
+        const cognitoUser = userPool.getCurrentUser();
+
+        if (!cognitoUser) {
+            displayUpdate('You are not signed in. Please sign in first.');
+            return;
+        }
+
+        const session = await new Promise((resolve, reject) => {
+            cognitoUser.getSession((err, sess) => {
+                if (err) reject(err);
+                else resolve(sess);
+            });
+        });
+
+        const currentUsername = cognitoUser.getUsername();
+        console.log("Current username for history:", currentUsername);
+
+        // Updated ajax call with dataType
+        const allRides = await $.ajax({
+            method: 'GET',
+            url: _config.api.invokeUrl + '/ride',
+            headers: {
+                Authorization: authToken
+            },
+            dataType: 'json'   // ← key fix
+        });
+
+        // No need for JSON.parse — allRides is already an array
+        const myRides = allRides.filter(ride => ride.User === currentUsername);
+
+        if (myRides.length === 0) {
+            displayUpdate('You haven’t taken any rides yet.<br>Request your first unicorn ride!');
+            return;
+        }
+
+        myRides.sort((a, b) => new Date(b.RequestTime) - new Date(a.RequestTime));
+
+        let html = '<strong style="font-size:1.1em">My Ride History</strong><ul class="ride-history">';
+
+        myRides.forEach(ride => {
+            const time = new Date(ride.RequestTime).toLocaleString('en-IN', {
+                timeZone: 'Asia/Kolkata',
+                dateStyle: 'medium',
+                timeStyle: 'short'
+            });
+
+            html += `
+                <li>
+                    <div><strong>${time}</strong></div>
+                    <div>Unicorn: ${ride.Unicorn.Name} (${ride.Unicorn.CarNumber})</div>
+                    <div>Gender: ${ride.Unicorn.Gender}</div>
+                </li>
+            `;
+        });
+
+        html += '</ul>';
+
+        displayUpdate(html);
+
+    } catch (err) {
+        console.error('History error:', err);
+        displayUpdate('Sorry, could not load ride history right now.<br>Error: ' + (err.message || 'Unknown error'));
+    }
+});
+});
 
     function handleRequestClick(event) {
-        var pickupLocation = WildRydes.map.selectedPoint;
         event.preventDefault();
-        requestUnicorn(pickupLocation);
+        const pickup = WildRydes.map.pickup;
+        const dropoff = WildRydes.map.dropoff;
+        if (pickup && dropoff) {
+            requestUnicorn(pickup, dropoff);
+        } else {
+            alert("Please select both pickup and drop-off locations.");
+        }
     }
 
-    function animateArrival(callback) {
-        var dest = WildRydes.map.selectedPoint;
-        var origin = {};
+function animateArrival(callback) {
+    // Use pickup location as destination (unicorn comes to pickup)
+    var dest = WildRydes.map.pickup;
 
-        if (dest.latitude > WildRydes.map.center.latitude) {
-            origin.latitude = WildRydes.map.extent.minLat;
-        } else {
-            origin.latitude = WildRydes.map.extent.maxLat;
-        }
-
-        if (dest.longitude > WildRydes.map.center.longitude) {
-            origin.longitude = WildRydes.map.extent.minLng;
-        } else {
-            origin.longitude = WildRydes.map.extent.maxLng;
-        }
-
-        WildRydes.map.animate(origin, dest, callback);
+    if (!dest) {
+        console.warn("No pickup location set for animation");
+        callback(); // skip animation if no pickup
+        return;
     }
+
+    // Choose a starting point outside the current view (simple logic)
+    var origin = {};
+    if (dest.latitude > WildRydes.map.center.latitude) {
+        origin.latitude = WildRydes.map.extent.minLat - 0.05; // slightly outside
+    } else {
+        origin.latitude = WildRydes.map.extent.maxLat + 0.05;
+    }
+
+    if (dest.longitude > WildRydes.map.center.longitude) {
+        origin.longitude = WildRydes.map.extent.minLng - 0.05;
+    } else {
+        origin.longitude = WildRydes.map.extent.maxLng + 0.05;
+    }
+
+    //WildRydes.map.animate(origin, dest, callback);
+    WildRydes.map.animate(origin, dest, function () {
+        // animation finished
+        callback();
+    });
+}
+
+function completeRequest(result) {
+    var unicorn = result.Unicorn;
+    var pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+
+    displayUpdate(
+        `Driver: ${unicorn.Name}, your car (${unicorn.CarNumber}) is on ${pronoun} way to pickup.`
+    );
+
+    console.log("Starting unicorn animation now...");  // ← ADD THIS LINE
+    animateArrival(function () {
+        console.log("Unicorn animation finished!");   // ← ADD THIS
+        displayUpdate(`${unicorn.Name} has arrived at pickup! Hop in!`);
+        if (WildRydes.map.clearGraphics) {
+            WildRydes.map.clearGraphics('unicorn');
+        } else {
+            console.warn("clearGraphics not available");
+        }
+        $('#request').prop('disabled', true).text('Ride In Progress');
+        $('#newRide').show();
+    });
+}
 
     function displayUpdate(text) {
         var updates = $('#updates');
@@ -155,7 +303,7 @@ WildRydes.map = WildRydes.map || {};
         if (updates.children().length > 10) {
             updates.children().first().remove();
         }
-        var panelBody = updates.parent(); // this is .panel-body
+        var panelBody = updates.parent();
         panelBody.scrollTop(panelBody[0].scrollHeight);
     }
 }(jQuery));
